59c792d3882b3f574448eabb85ad52dabd746fc6
Azamat <azamat@aza.local>
2024-07-19 01:54:30 +0500

    Winners page

--- app/api/api.ts
+++ app/api/api.ts
@@ -2,8 +2,10 @@ export const baseUrl = 'http://127.0.0.1:3000'
 
 const garageUrl = `${baseUrl}/garage`
 const engineUrl = `${baseUrl}/engide`
+const winnersUrl = `${baseUrl}/winners`
 
 export let countAllCars = 0
+export let countAllWinners = 0
 
 export const getCars = async (page: number = 0, limit: number = 10) => {
   try {
@@ -14,6 +16,21 @@ export const getCars = async (page: number = 0, limit: number = 10) => {
       throw new Error('Failed to fetch data')
     }
     countAllCars = Number(response.headers.get('X-Total-count'))
+
+    return await response.json()
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+export const getCarId = async (id: number) => {
+  try {
+    const response = await fetch(`${garageUrl}/${id}`, {
+      method: 'GET',
+    })
+    if (!response.ok) {
+      throw new Error('Failed to fetch data')
+    }
 
     return await response.json()
   } catch (error) {
@@ -69,4 +86,21 @@ export const driveMotorAPI = async (id: number) => {
     method: 'PATCH',
   }).catch()
   return res.status !== 200 ? { success: false } : { ...(await res.json()) }
+}
+
+export const getWinners = async (page: number = 0, limit = 10) => {
+  try {
+    const response = await fetch(
+      `${winnersUrl}?_page=${page}&_limit=${limit}`,
+      { method: 'GET' }
+    )
+    if (!response.ok) {
+      throw new Error('Failed to fetch data')
+    }
+    countAllWinners = Number(response.headers.get('X-Total-count'))
+
+    return await response.json()
+  } catch (error) {
+    console.error(error)
+  }
 }
--- app/winners/page.tsx
+++ app/winners/page.tsx
@@ -1,7 +1,75 @@
-export default function Winners() {
+'use client'
+
+import Image from 'next/image'
+import { useEffect, useState } from 'react'
+import CarItem from '../component/Home/CarItem'
+import { Car, CarItemProps } from '../types/Car'
+import { countAllWinners, getCarId, getWinners } from '../api/api'
+import { getRandomColor, getRandomName } from '../utils/util'
+import WinnersCarItem from '../component/Winners/WinnersCarItem'
+
+export default function Home() {
+  const [data, setData] = useState<Car[]>([])
+  const [page, setPage] = useState(1)
+
+  const fetchData = async () => {
+    try {
+      const data = await getWinners(page, 10)
+      data.map(async (item) => {
+        const newData = await getCarId(item.id)
+        item.name = newData.name
+        item.color = newData.color
+      })
+      setData(data)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [page])
+
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      winners
+    <main className='flex min-h-screen flex-col p-24'>
+      <h5 className='text-2xl uppercase text-gray-700'>WINNERS</h5>
+      <div className='flex items-center justify-between '>
+        <h5 className='text-2xl uppercase text-gray-700 text-center'>#</h5>
+        <h5 className='text-2xl uppercase text-gray-700 text-center'>CAR</h5>
+        <h5 className='text-2xl uppercase text-gray-700 text-center'>NAME</h5>
+        <h5 className='text-2xl uppercase text-gray-700 text-center'>WINS</h5>
+        <h5 className='text-2xl uppercase text-gray-700 text-center'>TIME</h5>
+      </div>
+      <hr className='h-px my-8 bg-purple-700 border-0 dark:bg-gray-700' />
+      {data.length > 0 &&
+        data.map((car) => <WinnersCarItem key={car.id} car={car} />)}
+      <hr className='h-px my-8 bg-purple-700 border-0 dark:bg-gray-700' />
+      <div className='flex justify-between'>
+        <h5 className='text-2xl uppercase text-gray-700 '>
+          Cars ({countAllWinners})
+        </h5>
+        <div className='flex justify-center items-center'>
+          <button
+            type='button'
+            onClick={() => {
+              if (page > 1) setPage(page - 1)
+            }}
+            className=' text-purple-700 hover:text-white border border-purple-700 hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-purple-400 dark:text-purple-400 dark:hover:text-white dark:hover:bg-purple-500 dark:focus:ring-purple-900'
+          >
+            PREV {page > 1 && page - 1}
+          </button>
+          <h5 className='text-xl uppercase text-gray-700 mx-8'>#{page}</h5>
+          <button
+            type='button'
+            onClick={() => {
+              if (data.length > 9) setPage(page + 1)
+            }}
+            className='text-purple-700 hover:text-white border border-purple-700 hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-purple-400 dark:text-purple-400 dark:hover:text-white dark:hover:bg-purple-500 dark:focus:ring-purple-900'
+          >
+            NEXT {data.length > 9 && page + 1}
+          </button>
+        </div>
+      </div>
     </main>
   )
 }
